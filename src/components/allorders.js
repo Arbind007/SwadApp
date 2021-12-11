b7560acf1ebeaea74b9873a4550eb4d23ae499c7
ARBIND LOCHAN MISHRA <almbbsr456@gmail.com>
2021-12-12 00:15:08 +0530

    :bug: allorder

--- src/components/allorders.js
+++ src/components/allorders.js
@@ -14,31 +14,39 @@ function Allorder() {
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  });
   return (
     <div>
-    <div className="text-center my-5 ">
-     <h1> All Orders </h1>
-     </div>
-    <table className="table table-striped ">
-    <thead className="bg-dark text-white">
-    <tr>
-      <th scope="col"><h3>Name</h3></th>
-      <th scope="col"><h3>Email</h3></th>
-      <th scope="col"><h3>Address</h3></th>
-      <th scope="col"><h3>Orders</h3></th>
-    </tr>
-    </thead>
-    <tbody>
-        {posts.map((post) => (
-          <tr className="">
-          <th scope="row">{post.name}</th>
-          <th scope="row">{post.email}</th>
-          <th scope="row">{post.address}</th>
-          <th scope="row">{post.order}</th>
+      <div className="text-center my-5 ">
+        <h1> All Orders </h1>
+      </div>
+      <table className="table table-striped ">
+        <thead className="bg-dark text-white">
+          <tr>
+            <th scope="col">
+              <h3>Name</h3>
+            </th>
+            <th scope="col">
+              <h3>Email</h3>
+            </th>
+            <th scope="col">
+              <h3>Address</h3>
+            </th>
+            <th scope="col">
+              <h3>Orders</h3>
+            </th>
           </tr>
-        ))}
-      </tbody>
+        </thead>
+        <tbody>
+          {posts.map((post) => (
+            <tr className="">
+              <th scope="row">{post.name}</th>
+              <th scope="row">{post.email}</th>
+              <th scope="row">{post.address}</th>
+              <th scope="row">{post.order}</th>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
